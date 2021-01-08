b79b1d64fdcdfb3bf8e47ad9c93a6e888d9741de
purnima <pooyax14@gmail.com>
2021-01-08 14:39:59 +0530

    Add connection string for mongodb atlas
    - updating database config file for production environment

--- config/database.js
+++ config/database.js
@@ -4,17 +4,11 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi-socket-backend'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        uri: env('DATABASE_URI'),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: true
       },
     },
   },
-});
+})
